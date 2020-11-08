9dbce3d178b8e7b10b72086b7c9ed1eb07671a47
yassu-356 <72985474+yassu-356@users.noreply.github.com>
2020-11-09 08:31:14 +0900

    最低限の初期設定のみ追加

--- symmetricds/sql/init_sym_tables.sql
+++ symmetricds/sql/init_sym_tables.sql
@@ -33,7 +33,6 @@ insert into sym_node_group (node_group_id) values ('child');
 ------------------------------------------------------------------------------
 
 insert into sym_node_group_link (source_node_group_id, target_node_group_id, data_event_action) values ('root', 'child', 'W');
-insert into sym_node_group_link (source_node_group_id, target_node_group_id, data_event_action) values ('child', 'root', 'P');
 
 ------------------------------------------------------------------------------
 -- Triggers
@@ -41,42 +40,16 @@ insert into sym_node_group_link (source_node_group_id, target_node_group_id, dat
 
 insert into sym_trigger
 (trigger_id,source_table_name,channel_id,last_update_time,create_time)
-values('item_selling_price','item_selling_price','default',current_timestamp,current_timestamp);
-
-insert into sym_trigger
-(trigger_id,source_table_name,channel_id,last_update_time,create_time)
 values('item','item','default',current_timestamp,current_timestamp);
-
-insert into sym_trigger
-(trigger_id,source_table_name,channel_id,last_update_time,create_time)
-values('sale_transaction','sale_transaction','default',current_timestamp,current_timestamp);
-
-insert into sym_trigger
-(trigger_id,source_table_name,channel_id,last_update_time,create_time)
-values('sale_return_line_item','sale_return_line_item','default',current_timestamp,current_timestamp);
-
--- Triggers with capture disabled, so they are used for initial load only
-insert into sym_trigger
-(trigger_id,source_table_name,channel_id, sync_on_insert, sync_on_update, sync_on_delete,last_update_time,create_time)
-values('sale_transaction_corp','sale_transaction','default',0,0,0,current_timestamp,current_timestamp);
-
-insert into sym_trigger
-(trigger_id,source_table_name,channel_id, sync_on_insert, sync_on_update, sync_on_delete,last_update_time,create_time)
-values('sale_return_line_item_corp','sale_return_line_item','default',0,0,0,current_timestamp,current_timestamp);
 
 ------------------------------------------------------------------------------
 -- Routers
 ------------------------------------------------------------------------------
 
--- Default router sends all data from corp to store
 insert into sym_router
 (router_id,source_node_group_id,target_node_group_id,router_type,create_time,last_update_time)
 values('root_2_child', 'root', 'child', 'default',current_timestamp, current_timestamp);
 
--- Default router sends all data from store to corp
-insert into sym_router
-(router_id,source_node_group_id,target_node_group_id,router_type,create_time,last_update_time)
-values('child_2_root', 'child', 'root', 'default',current_timestamp, current_timestamp);
 
 ------------------------------------------------------------------------------
 -- Trigger Routers
@@ -87,25 +60,3 @@ insert into sym_trigger_router
 (trigger_id,router_id,initial_load_order,last_update_time,create_time)
 values('item','root_2_child', 100, current_timestamp, current_timestamp);
 
--- Send item prices to associated stores
-insert into sym_trigger_router
-(trigger_id,router_id,initial_load_order,initial_load_select,last_update_time,create_time)
-values('item_selling_price','root_2_child',100,'store_id=''$(externalId)''',current_timestamp,current_timestamp);
-
--- Send all sales transactions to corp
-insert into sym_trigger_router
-(trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('sale_transaction','child_2_root', 200, current_timestamp, current_timestamp);
-
-insert into sym_trigger_router
-(trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('sale_return_line_item','child_2_root', 200, current_timestamp, current_timestamp);
-
--- Send all sales transactions to store during initial load
-insert into sym_trigger_router
-(trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('sale_transaction_corp','root_2_child', 200, current_timestamp, current_timestamp);
-
-insert into sym_trigger_router
-(trigger_id,router_id,initial_load_order,last_update_time,create_time)
-values('sale_return_line_item_corp','root_2_child', 200, current_timestamp, current_timestamp);
